6989500c80586b75560276ec114e3934bd3ac9a2
creibaud <creibaud@guardiaschool.fr>
2024-02-03 11:20:25 +0100

    Delete unused images and update role images

--- main.cpp
+++ main.cpp
@@ -13,13 +13,23 @@ int main() {
     sf::Image *backRole = new sf::Image();
     backRole->loadFromFile("assets/images/backRole.png");
 
-    sf::Image *backRole2 = new sf::Image();
-    backRole2->loadFromFile("assets/images/backRole2.png");
+    sf::Image *shogunImage = new sf::Image();
+    shogunImage->loadFromFile("assets/images/shogun_verso.png");
+
+    sf::Image *samuraiImage = new sf::Image();
+    samuraiImage->loadFromFile("assets/images/samurai_verso.png");
+
+    sf::Image *ninjaImage = new sf::Image();
+    ninjaImage->loadFromFile("assets/images/ninja_verso.png");
+
+    sf::Image *roninImage = new sf::Image();
+    roninImage->loadFromFile("assets/images/ronin_verso.png");
 
     std::vector<sf::Image*> *roleImages = new std::vector<sf::Image*>();
-    for (int i = 0; i < 7; i++) {
-        roleImages->push_back(backRole2);
-    }
+    roleImages->push_back(shogunImage);
+    roleImages->push_back(samuraiImage);
+    roleImages->push_back(ninjaImage);
+    roleImages->push_back(roninImage);
 
     std::vector<sf::Image*> *images = new std::vector<sf::Image*>();
     for (int i = 0; i < 12; i++) {
